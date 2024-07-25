cd093bfe2a74a1552d79a0b85de1d46819a1c225
mashmool <ap4games2017@gmail.com>
2024-07-25 20:59:45 +0330

    implement Data base of menus (models)

--- Landing/models.py
+++ Landing/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class FoodsField(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class SectionFood(models.Model):
+    foods = models.ForeignKey(FoodsField, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
+class Food(models.Model):
+    section_food = models.ForeignKey(SectionFood, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
