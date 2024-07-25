fe976e5ff37737135d717cd14c2538b754f1b88f
mashmool <ap4games2017@gmail.com>
2024-07-25 21:12:31 +0330

    add template tag for money - and Connect models to admin

--- Landing/admin.py
+++ Landing/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FoodsField, Food, SectionFood
 
 # Register your models here.
+admin.site.register(Food)
+admin.site.register(SectionFood)
+admin.site.register(FoodsField)
--- Landing/models.py
+++ Landing/models.py
@@ -7,13 +7,22 @@ from django.db import models
 class FoodsField(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class SectionFood(models.Model):
-    foods = models.ForeignKey(FoodsField, on_delete=models.CASCADE)
+    foods = models.ForeignKey(FoodsField, on_delete=models.CASCADE,related_name='section_food')
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Food(models.Model):
     section_food = models.ForeignKey(SectionFood, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    price = models.IntegerField()
+    price = models.IntegerField(help_text="لطفا قیمت را به شکل تومان وارد کنید مثلا ۹۰")
+
+    def __str__(self):
+        return self.name + "  " + str(self.price) + " toman"
